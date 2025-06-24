c143271f2bbca6128721ad94718c0178befdcf7e
Sidharth <osam.sid@gmail.com>
2025-06-25 00:15:21 +0530

    Update render_flask_wrapper.py

--- render_flask_wrapper.py
+++ render_flask_wrapper.py
@@ -1,16 +1,9 @@
-from flask import Flask
-import threading
-app = Flask(__name__)
+import gradio as gr
+from portrait_gui_app import portrait_ui
+import os
 
-def launch_gui():
-    import portrait_gui_app
-    portrait_gui_app.app.launch(server_name="0.0.0.0", server_port=7860)
-
-threading.Thread(target=launch_gui).start()
-
-@app.route("/")
-def home():
-    return "✅ Gradio app running on :7860"
+def start_gradio():
+    portrait_ui.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 3000)))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    start_gradio()
